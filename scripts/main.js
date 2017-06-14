aeb7f5b011beb1be787ce9bd51237f3c10ef756e
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-06-14 21:50:37 +0100

    Playing around with JSON and AJAX for now

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,25 @@
-$(document).ready(function(){
-    $("#term").focus(function() {
-        let full = $("#exercise").has("img").length ? true : false;
-        if (full == false) {
-            $("#exercise").empty();
-        }
-    })
+"use strict";
+$(document).ready(function() {
 
     let getExercise = function() {
         let exercise = $("#term").val();
 
-        if (film == "") {
+        if (exercise == "") {
             $("#exercise").html("<h2 class='loading'>Please enter something.</h2>")
         } else {
             $("#exercise").html("<h2 class='loading'>Exercise is on its way.</h2>")
+
+            $.getJSON("https://wger.de/api/v2/exercise/?name=" + exercise + "&format=json&language=2", function(json) {
+                $('#exercise').html('<h2 class="loading">Exercise found!</h2>' + json.results[0].description);
+            })
         }
     }
+
+    $("#search").click(getExercise);
+
+    $("#term").keyup(function(event) {
+        if (event.keycode == 13) {
+            getExercise();
+        }
+    })
 })
